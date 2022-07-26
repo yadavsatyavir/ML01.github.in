3cc432e1811b70473a56f015e31e10b997e6c589
yadavsatyavir <yadav.satyavir@gmail.com>
2022-07-26 10:34:21 +0530

    rename

--- main.py
+++ main.py
@@ -355,4 +355,4 @@ def nlp_convert_english_to_french():
     return result, 200
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='127.0.0.1', port=8080, debug=True)
